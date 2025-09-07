81f6799e8250d7deafac39ccfb17cab383e7655f
Hasnain Ahamed Nisan <68366713+hasnain-nisan@users.noreply.github.com>
2025-09-07 11:56:01 +0600

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     NEXTAUTH_SECRET: "your-secret-key",
-    API_BASE_URL: "http://localhost:4000/api/v1",
-    NEXT_PUBLIC_APP_NAME: "Notepad App",
+    // API_BASE_URL: "http://localhost:4000/api/v1",
+    // NEXTAUTH_URL: "http://localhost:3000",
+    API_BASE_URL: "https://tp-nest-backend.onrender.com/api/v1",
+    NEXTAUTH_URL: "https://tp-admin-mocha.vercel.app",
+    NEXT_PUBLIC_APP_NAME: "TP Admin",
   },
 };
 
