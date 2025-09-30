b5a89b2bcbcdc0ae3d2a017d27f4e9fa280a4de8
Hasnain Ahamed Nisan <68366713+hasnain-nisan@users.noreply.github.com>
2025-09-30 22:21:21 +0600

    Review done
    1. Interview specific fields changed to checkbox from url
    2. Added role and team to stakeholder
    3. Project config and global config
    4. Based on pdf design modification like font, color, logo etc

--- components/ui/BrandLogo.tsx
+++ components/ui/BrandLogo.tsx
@@ -1,6 +1,7 @@
 import { GalleryVerticalEnd } from "lucide-react";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 interface BrandLogoProps {
   readonly href?: string;
@@ -13,20 +14,28 @@ interface BrandLogoProps {
 export function BrandLogo({
   href = "/",
   label = "Transparent Partners",
-  iconSize = 16,
-  containerSize = 24,
+  iconSize = 25,
+  containerSize = 25,
   className,
 }: BrandLogoProps) {
+  const logoPath = "/tp-logo.png";
   return (
     <Link
       href={href}
       className={cn("flex items-center gap-2 font-medium", className)}
     >
       <div
-        className="bg-primary text-primary-foreground flex items-center justify-center rounded-md"
+        className="text-primary-foreground flex items-center justify-center rounded-md"
         style={{ width: containerSize, height: containerSize }}
       >
-        <GalleryVerticalEnd style={{ width: iconSize, height: iconSize }} />
+        {/* <GalleryVerticalEnd style={{ width: iconSize, height: iconSize }} /> */}
+        <Image
+          src={logoPath}
+          alt="Transparent Partners Logo"
+          width={iconSize}
+          height={iconSize}
+          className="rounded-sm" // Optional: Add a class if your logo should be smaller than the container
+        />
       </div>
       {label}
     </Link>
